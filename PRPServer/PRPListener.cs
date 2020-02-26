566fe21614a7f6c1889cfdebc7f932e010edb7e1
Peter Ho <PupilStudio@outlook.com>
2020-02-26 19:08:57 +0800

    Init Submit

---
+++ PRPServer/PRPListener.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PRPServer
+{
+    public class PRPListener
+    {
+        IPEndPoint listenEndPoint;
+        Socket listener;
+
+        PRPListener(int port)
+        {
+            listenEndPoint.Address = IPAddress.Any;
+            listenEndPoint.Port = port;
+        }
+        
+        PRPListener(IPEndPoint endPoint)
+        {
+            listenEndPoint = endPoint;
+        }
+
+        PRPListener(string localIpAddr, int port = 7207)
+        {
+            listenEndPoint.Address = IPAddress.Parse(localIpAddr);
+            listenEndPoint.Port = port;
+        }
+
+        void Listen(int backlog = 512)
+        {
+            listener = new Socket(AddressFamily.InterNetwork & AddressFamily.InterNetworkV6,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+            listener.Bind(listenEndPoint);
+            listener.Listen(backlog);
+        }
+    }
+}
---
+++ PRPServer/PRPRegister.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRPServer
+{
+    class NameRegisteredException : Exception
+    {
+
+    }
+
+    struct RegisterInfo
+    {
+        public string RegisterName;
+        public IPEndPoint EndPoint;
+    }
+
+    class PRPRegister
+    {
+        private List<RegisterInfo> registers;
+
+        void Register(string name, IPEndPoint endPoint)
+        {
+            if ((from question in registers where question.RegisterName == name select question).Count() != 0)
+            {
+                throw new NameRegisteredException();
+            }
+            RegisterInfo info = new RegisterInfo();
+            info.RegisterName = name;
+            info.EndPoint = endPoint;
+            registers.Add(info);
+        }
+
+        void Unregister(string name)
+        {
+            registers.RemoveAll((RegisterInfo info) =>
+            {
+                return info.RegisterName == name;
+            });
+        }
+
+        IPEndPoint GetService(string name)
+        {
+            return registers.Find((RegisterInfo inf) =>
+            {
+                return inf.RegisterName == name;
+            }).EndPoint;
+        }
+    }
+}
